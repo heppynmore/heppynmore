042c46542c983c270ff012b6eb1a9334c52775e2
Caterina <caterina.vernieri@cern.ch>
2015-11-28 13:44:37 -0600

    adding HH selection

---
+++ python/hh13TeVconfig/run_prepare.sh
@@ -0,0 +1,12 @@
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S QCD500
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S QCD700
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S QCD1000
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S QCD1500
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S WQQ
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S TT
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S WZ
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S ZZ
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S WW
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S JetHT
+./prepare_environment_with_config.py -C heppy13TeVconfig/paths -C heppy13TeVconfig/samples_nosplit.cfg -S QCD2000
+
