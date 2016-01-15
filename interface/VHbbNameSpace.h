42805ec803042bace80ccc6f78e9fbfe50ae230d
Benjamin <benjaminkreis@gmail.com>
2016-01-14 20:23:37 -0600

    Adding new Zmumu and Wmunu directories from Debarati.  New DY reweighting in namespace.  Update Znunu variables to plot

--- interface/VHbbNameSpace.h
+++ interface/VHbbNameSpace.h
@@ -274,9 +274,28 @@ namespace VHbb {
   }
 
   // weights correction for EWK NLO correction
-  double ptWeightZllH( double genHPt){
+  double ptWeightZllH(int nGenVbosons,double GenVbosons_pt,int VtypeSim,int GenVbosons_pdgId){
     double SF = 1.;
-    if ( genHPt > 1.) SF = 0.999757-0.000174527*genHPt;
+    if (nGenVbosons ==1)
+      {
+        if (VtypeSim == 0 || VtypeSim == 1 || VtypeSim == 4 || VtypeSim == 5)
+          {
+            if (GenVbosons_pdgId == 23)
+              {
+                //for Z options
+                if (GenVbosons_pt > 100. && GenVbosons_pt < 3000) SF = -0.1808051+6.04146*(TMath::Power((GenVbosons_pt+759.098),-0.242556));
+              }
+          }
+        else if (VtypeSim == 2 || VtypeSim == 3)
+          {
+            //for W options
+            if (GenVbosons_pdgId == 24 || GenVbosons_pdgId == -24)
+              {
+                if (GenVbosons_pt > 100. && GenVbosons_pt < 3000) SF = -0.830041+7.93714*(TMath::Power((GenVbosons_pt+877.978),-0.213831));
+              }
+          }
+      }
+    //std::cout << " SF " << " " << SF << std::endl;
     return SF>0?SF:0;
   }
 
